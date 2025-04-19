05b1d204734e3aaa14daf8c606b2e22a5238f2d6
Iagodizlla <iagoschlemper@gmail.com>
2025-04-19 02:18:49 -0300

    Implementado (cadastrar, editar, listar, remover) automovel

--- Concessionaria/Menu.cs
+++ Concessionaria/Menu.cs
@@ -6,29 +6,41 @@ public class Menu
     {
         Console.Clear();
         Console.WriteLine("-----------------------------");
-        Console.WriteLine("      CONCESSIONARIA");
+        Console.WriteLine("        CONCESSIONARIA");
         Console.WriteLine("-----------------------------\n");
     }
     public static char MenuMotocicleta()
     {
         Cabecalho();
-        Console.WriteLine("Escolha uma opcao: ");
         Console.WriteLine("1. Cadastrar");
         Console.WriteLine("2. Editar");
         Console.WriteLine("3. Remover");
         Console.WriteLine("4. Listar");
         Console.WriteLine("S. Voltar");
+        Console.Write("Escolha uma opcao: ");
+        char opcao = Console.ReadLine()!.ToUpper()[0];
+        return opcao;
+    }
+    public static char MenuAutomovel()
+    {
+        Cabecalho();
+        Console.WriteLine("1. Cadastrar");
+        Console.WriteLine("2. Editar");
+        Console.WriteLine("3. Remover");
+        Console.WriteLine("4. Listar");
+        Console.WriteLine("S. Voltar");
+        Console.Write("Escolha uma opcao: ");
         char opcao = Console.ReadLine()!.ToUpper()[0];
         return opcao;
     }
     public static char MenuPrincipal()
     {
         Cabecalho();
-        Console.WriteLine("Escolha uma opcao: ");
         Console.WriteLine("1. Automovel");
         Console.WriteLine("2. Caminhonete");
         Console.WriteLine("3. Motocicleta");
         Console.WriteLine("S. Sair");
+        Console.Write("Escolha uma opcao: ");
         char opcao = Console.ReadLine()!.ToUpper()[0];
         return opcao;
     }
--- Concessionaria/Program.cs
+++ Concessionaria/Program.cs
@@ -12,7 +12,24 @@ public class Program
             Menu.Cabecalho();
             char opcao = Menu.MenuPrincipal();
             bool sair = false;
-            if (opcao == '3')
+            if(opcao == '1')
+            {
+                while (sair == false)
+                {
+                    Menu.Cabecalho();
+                    char opcaoS = Menu.MenuAutomovel();
+                    switch (opcaoS)
+                    {
+                        case '1': CadastrarAutomovel(); break;
+                        case '2': EditarAutomovel(); break;
+                        case '3': RemoverAutomovel(); break;
+                        case '4': ListarAutomoveis(); break;
+                        case 'S': sair = true; break;
+                        default: Console.WriteLine("Opcao invalida! Pressione qualquer tecla para continuar..."); break;
+                    }
+                }
+            }
+            if(opcao == '3')
             {
                 while (sair == false)
                 {
@@ -31,13 +48,157 @@ public class Program
             }
         }
     }
+    #region Automovel
+    public static void CadastrarAutomovel()
+    {
+        Menu.Cabecalho();
+        Console.WriteLine("Cadastrar Automovel");
+        Console.WriteLine("---------------------");
+
+        Automovel automovel = ObterDadosAutomovel();
+        AdicionarVeiculo(automovel);
+    }
+    public static Automovel ObterDadosAutomovel()
+    {
+        Console.Write("Marca: ");
+        string marca = Console.ReadLine()!;
+        Console.Write("Modelo: ");
+        string modelo = Console.ReadLine()!;
+        Console.Write("Ano de Fabricacao: ");
+        int anoFabricacao = int.Parse(Console.ReadLine()!);
+        Console.Write("Cor: ");
+        string cor = Console.ReadLine()!;
+        Console.Write("Capacidade de Ocupantes: ");
+        int capacidadeOcupantes = int.Parse(Console.ReadLine()!);
+        Console.Write("Combustivel: ");
+        string combustivel = Console.ReadLine()!;
+        Console.Write("Cambio: ");
+        string cambio = Console.ReadLine()!;
+        Console.Write("Capacidade do Tanque: ");
+        double capacidadeTanque = double.Parse(Console.ReadLine()!);
+        Console.Write("Tipo de Freio: ");
+        string tipoFreio = Console.ReadLine()!;
+        Console.Write("Preco: ");
+        double preco = double.Parse(Console.ReadLine()!);
+        Console.Write("Numero de Portas: ");
+        int numeroPortas = int.Parse(Console.ReadLine()!);
+        Console.Write("Ar Condicionado (true/false): ");
+        bool arCondicionado = bool.Parse(Console.ReadLine()!);
+        Console.Write("Sistema de Som (true/false): ");
+        bool sistemaSom = bool.Parse(Console.ReadLine()!);
+        Console.Write("Airbags (true/false): ");
+        bool airbags = bool.Parse(Console.ReadLine()!);
+        Console.Write("Estepe (true/false): ");
+        bool estepe = bool.Parse(Console.ReadLine()!);
+        Console.Write("Carroceria (Hatch/Sedan): ");
+        string carroceria = Console.ReadLine()!;
+        string placa = anoFabricacao.ToString().Substring(0, 2) + marca.Substring(1, 1) + anoFabricacao.ToString().Substring(1, 1) + "-" +
+            cor.Substring(0, 2) + modelo.Substring(0, 2);
+        Automovel automovel = new Automovel(marca, modelo, anoFabricacao, cor, capacidadeOcupantes, combustivel, cambio, capacidadeTanque, 
+            tipoFreio, preco, numeroPortas, arCondicionado, sistemaSom, airbags, estepe, carroceria, placa);
+        return automovel;
+    }
+    public static void ListarAutomoveis()
+    {
+        Menu.Cabecalho();
+        Console.WriteLine("Listar Automovel");
+        Console.WriteLine("------------------");
+        Console.WriteLine(
+            "{0, -10} | {1, -20} | {2, -20} | {3, -15} | {4, -10} | {5, -15}",
+            "Placa", "Marca", "Modelo", "Ano Fabricacao", "Preco", "Numero de portas"
+            );
+
+        Automovel[] automoveis = BuscarAutomoveis();
+        if (automoveis != null)
+        {
+            for (int i = 0; i < automoveis.Length; i++)
+            {
+                if (automoveis[i] == null) continue;
+
+                Console.WriteLine(
+                "{0, -10} | {1, -20} | {2, -20} | {3, -15} | {4, -10} | {5, -15}",
+                    automoveis[i].Placa, automoveis[i].Marca, automoveis[i].Modelo, automoveis[i].AnoFabricacao, automoveis[i].Preco, automoveis[i].NumeroPortas
+                );
+            }
+            Console.ReadLine();
+        }
+    }
+    public static Automovel[] BuscarAutomoveis()
+    {
+        Automovel[] automoveis = new Automovel[Veiculos.Count];
+        if (Veiculos.Count == 0)
+        {
+            Console.WriteLine("Nao existe veiculos.");
+            Console.ReadLine();
+            return null!;
+        }
+        for (int i = 0; i < Veiculos.Count; i++)
+        {
+            if (Veiculos[i] == null) continue;
+            if (Veiculos[i] is Automovel)
+            {
+                automoveis[i] = (Automovel)Veiculos[i];
+            }
+        }
+        return automoveis;
+    }
+    public static void RemoverAutomovel()
+    {
+        Menu.Cabecalho();
+        Console.WriteLine("Remover Automovel");
+        Console.WriteLine("-------------------");
+
+        ListarAutomoveis();
+        Console.WriteLine("Digite a placa da automovel: ");
+        string placa = Console.ReadLine()!;
+        Veiculo veiculo = BuscarVeiculo(placa);
+        if (veiculo is Automovel)
+        {
+            RemoverVeiculo(veiculo);
+            Console.WriteLine("Automovel removida com sucesso!");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Automovel nao encontrada!");
+            Console.ReadLine();
+        }
+    }
+    public static void EditarAutomovel()
+    {
+        Menu.Cabecalho();
+        Console.WriteLine("Editar Automovel");
+        Console.WriteLine("------------------");
+
+        ListarMotocicleta();
+        Console.WriteLine("Digite a placa da automovel: ");
+        string placa = Console.ReadLine()!;
+        Veiculo veiculoAntigo = BuscarVeiculo(placa);
+        if (veiculoAntigo is Automovel)
+        {
+            RemoverVeiculo(veiculoAntigo);
+            Menu.Cabecalho();
+            Console.WriteLine("Editar Automovel");
+            Console.WriteLine("---------------------");
+
+            Automovel automovel = ObterDadosAutomovel();
+            AdicionarVeiculo(automovel);
+        }
+        else
+        {
+            Console.WriteLine("Automovel nao encontrada!");
+            Console.ReadLine();
+        }
+    }
+    #endregion
+    #region Motocicleta
     public static void ListarMotocicleta()
     {
         Menu.Cabecalho();
         Console.WriteLine("Listar Motocicleta");
         Console.WriteLine("------------------");
         Console.WriteLine(
-            "{0, -10} | {1, -20} | {2, -20} | {3, -10} | {4, -10} | {5, -15}",
+            "{0, -10} | {1, -20} | {2, -20} | {3, -15} | {4, -10} | {5, -15}",
             "Placa", "Marca", "Modelo", "Ano Fabricacao", "Preco", "Cilindrada"
             );
 
@@ -49,7 +210,7 @@ public class Program
                 if (motocicletas[i] == null) continue;
 
                 Console.WriteLine(
-                "{0, -10} | {1, -20} | {2, -20} | {3, -10} | {4, -10} | {5, -15}",
+                "{0, -10} | {1, -20} | {2, -20} | {3, -15} | {4, -10} | {5, -15}",
                     motocicletas[i].Placa, motocicletas[i].Marca, motocicletas[i].Modelo, motocicletas[i].AnoFabricacao, motocicletas[i].Preco, motocicletas[i].Cilindrada
                 );
             }
@@ -63,7 +224,7 @@ public class Program
         Console.WriteLine("------------------");
 
         ListarMotocicleta();
-        Console.WriteLine("Digite a placa da motocicleta: ");
+        Console.WriteLine("Digite a placa da automovel: ");
         string placa = Console.ReadLine()!;
         Veiculo veiculoAntigo = BuscarVeiculo(placa);
         if(veiculoAntigo is Motocicleta)
@@ -118,45 +279,6 @@ public class Program
             capacidadeTanque, tipoFreio, preco, cilindrada, tipo, partida, placa);
         return motocicleta;
     }
-    public static void CadastrarMotocicleta()
-    {
-        Menu.Cabecalho();
-        Console.WriteLine("Cadastrar Motocicleta");
-        Console.WriteLine("---------------------");
-
-        Motocicleta motocicleta = ObterDadosMotocicleta();
-        AdicionarVeiculo(motocicleta);
-    }
-    public static void RemoverMotocicleta()
-    {
-        Menu.Cabecalho();
-        Console.WriteLine("Remover Motocicleta");
-        Console.WriteLine("-------------------");
-
-        ListarMotocicleta();
-        Console.WriteLine("Digite a placa da motocicleta: ");
-        string placa = Console.ReadLine()!;
-        Veiculo veiculo = BuscarVeiculo(placa);
-        if (veiculo is Motocicleta)
-        {
-            RemoverVeiculo(veiculo);
-            Console.WriteLine("Motocicleta removida com sucesso!");
-            Console.ReadLine();
-        }
-        else
-        {
-            Console.WriteLine("Motocicleta nao encontrada!");
-            Console.ReadLine();
-        }
-    }
-    public static void AdicionarVeiculo(Veiculo veiculo)
-    {
-        Veiculos.Add(veiculo);
-    }
-    public static void RemoverVeiculo(Veiculo veiculo)
-    {
-        Veiculos.Remove(veiculo);
-    }
     public static Motocicleta[] BuscarMotocicletas()
     {
         Motocicleta[] motocicletas = new Motocicleta[Veiculos.Count];
@@ -176,6 +298,47 @@ public class Program
         }
         return motocicletas;
     }
+    public static void CadastrarMotocicleta()
+    {
+        Menu.Cabecalho();
+        Console.WriteLine("Cadastrar Motocicleta");
+        Console.WriteLine("---------------------");
+
+        Motocicleta motocicleta = ObterDadosMotocicleta();
+        AdicionarVeiculo(motocicleta);
+    }
+    public static void RemoverMotocicleta()
+    {
+        Menu.Cabecalho();
+        Console.WriteLine("Remover Motocicleta");
+        Console.WriteLine("-------------------");
+
+        ListarMotocicleta();
+        Console.WriteLine("Digite a placa da automovel: ");
+        string placa = Console.ReadLine()!;
+        Veiculo veiculo = BuscarVeiculo(placa);
+        if (veiculo is Motocicleta)
+        {
+            RemoverVeiculo(veiculo);
+            Console.WriteLine("Motocicleta removida com sucesso!");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Motocicleta nao encontrada!");
+            Console.ReadLine();
+        }
+    }
+    #endregion
+    #region Veiculo
+    public static void RemoverVeiculo(Veiculo veiculo)
+    {
+        Veiculos.Remove(veiculo);
+    }
+    public static void AdicionarVeiculo(Veiculo veiculo)
+    {
+        Veiculos.Add(veiculo);
+    }
     public static Veiculo BuscarVeiculo(string placa)
     {
         for (int i = 0; i < Veiculos.Count; i++)
@@ -189,4 +352,5 @@ public class Program
         Console.ReadLine();
         return null!;
     }
+    #endregion
 }
